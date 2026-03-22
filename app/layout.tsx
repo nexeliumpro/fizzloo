387074f48d0bcd9bc7cfe2e46d7b4a6ce1b9bd37
nexeliumpro <nexeliumpro@gmail.com>
2026-03-22 21:37:56 +0100

    deploy: app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Fizzloo -- Bath Bombs Kids Love',
-  description: 'Fizzing bath bombs with surprise toys inside -- for kids who can\'t wait for bath time',
+  description: 'Fizzing bath bombs with surprise toys inside',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -12,11 +12,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800;900&family=Baloo+2:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </head>
-      <body style={{ fontFamily: "'Nunito', system-ui, sans-serif" }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
